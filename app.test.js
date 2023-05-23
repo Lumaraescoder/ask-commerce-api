92c8aa0781efbc806ee77e7e1efc068098eb9c7c
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2023-05-24 00:43:10 +0100

    Improved CORS implementation and enchanced Jest test environment configuration

--- app.test.js
+++ app.test.js
@@ -2,9 +2,18 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('Test the root path', () => {
+  
   test('It should return 200', () => {
     return request(app).get('/').then(response => {
       expect(response.statusCode).toBe(200);
     });
   });
+
+  it("should set the CORS headers correctly", async () => {
+    const response = await request(app)
+      .get("/")
+      .expect(200);
+    expect(response.headers["access-control-allow-methods"]).toBe("GET, PUT, POST, DELETE");
+  });
+
 });
